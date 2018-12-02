3ee3c5f6d484fff3a052debc177609fed22e0286
Callum Jones <cj@icj.me>
2018-12-02 17:10:00 +0000

    add canijailbreak client

---
+++ api/jailbreak.go
@@ -0,0 +1,30 @@
+package api
+
+import "github.com/cj123/canijailbreak.com/model"
+
+const CanIJailbreakURL = "https://canijailbreak.com/"
+
+// NewIPSWClient creates an IPSWClient. If client == nil, http.DefaultClient is used.
+func NewCanIJailbreakClient(apiBase string, httpClient HTTPClient) *CanIJailbreakClient {
+	return &CanIJailbreakClient{
+		client: newHTTPWrapper(apiBase, httpClient),
+	}
+}
+
+type CanIJailbreakClient struct {
+	client *ipswHTTPWrapper
+}
+
+func (c *CanIJailbreakClient) GetJailbreaks() (*model.JailbreakJSON, error) {
+	var jbs *model.JailbreakJSON
+
+	resp, _, err := c.client.makeRequest("jailbreaks.json", nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = parseJSON(resp, &jbs)
+
+	return jbs, err
+}
